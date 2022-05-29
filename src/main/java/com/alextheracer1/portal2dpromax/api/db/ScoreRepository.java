40044e90983ae7e0c1dea1a441d063b6e94c2d36
Alexander Graf <alex.ander.graf@icloud.com>
2022-05-29 21:25:59 +0200

    Leaderboard shows the top 20 scores now

--- src/main/java/com/alextheracer1/portal2dpromax/api/db/ScoreRepository.java
+++ src/main/java/com/alextheracer1/portal2dpromax/api/db/ScoreRepository.java
@@ -1,6 +1,7 @@
 package com.alextheracer1.portal2dpromax.api.db;
 
 import com.alextheracer1.portal2dpromax.api.entities.score.Score;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,4 +9,6 @@ public interface ScoreRepository extends MongoRepository<Score, String> {
   Optional<Score> findByUserId(String userId);
 
   boolean existsByUserId(String userId);
+
+  List<Score> findTop20ItemsByOrderByScoreDesc();
 }
